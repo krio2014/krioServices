b624153be48a5e16db59aff26cee774f4f38284e
Adam Evans <adamevans385@msn.com>
2017-06-23 20:33:19 +0100

    separated header, footer,carousel
    Separated these three files. Now using a php file for content and
    including the header and footer as required

---
+++ newPageTemplate.php
@@ -0,0 +1,13 @@
+<?php include 'header.html';
+    
+//----carousel goes here if wanted   
+
+//include 'carousel.html';
+echo'<div class="row" name="body content">';
+//----Content goes here
+echo'`dfnzdndab';
+
+
+          
+
+include 'footer.html';?>
---
+++ services.php
@@ -0,0 +1,15 @@
+<?php include 'header.html';
+    
+//content   
+//include 'carousel.html';
+echo'
+        <div class="row" name="header-title">
+              <h1 class="display-4">Our Services</h1>
+         </div>
+                <div class="row" name="body content">
+                ';
+
+
+          
+
+include 'footer.html';?>
